69099c95bb8570c6fa6c29ffd5d7ca8ba668ad47
not-first <fakerrdev@gmail.com>
2024-07-03 18:03:25 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# vite-biome-tailwind project template
+
+Simple expansion of Vite's react template to create TypeScript projects integrated with:
+
+- [BiomeJS](https://biomejs.dev/): A fast formatter and linter (replacing both ESLint and Prettier) written in rust.
+- [TailwindCSS](https://tailwindcss.com/): CSS utility that provides classes to apply inline styles to components
+
+## Contents
+
+The unchanged vite-generated files: `.gitignore`, `index.html`, `tsconfig.app.js`, `tsconfig.node.json`, `tsconfig.json`, `vite-config` files.
+
+A `package.json` (and `package-lock.json`) file with ESLint, Prettier and their dependencies removed. BiomeJS and TailwindCSS are installed as devDependencies. Available scripts are:
+
+- `dev`, `build`, `preview` as provided by Vite
+- `format`: formats the project using Biome's formatter
+- `lint`: lints the project using Biome's linter
+
+Tailwind config files, including the default `tailwind.config.js` and `postcss.config.js`.
+
+Biome config file `biome.json`. In addition to default config, it has git integration enabled, and has the experimental useSortedClasses (sorting of tailwind classes) rule enabled.
+
+## Usage
+
+1. Clone to your machine. Do this by:
+    - Copying using [degit](https://github.com/Rich-Harris/degit) to download the files in the repo without the git history. (RECOMMENDED)
+      - `npx degit not-first/vite-biome-tailwind-template project-name`
+
+    - Cloning this repo to your machine. This will keep git history.
+       - `git clone https://github.com/not-first/vite-biome-tailwind-template.git`
+
+    - Creating a repo using this template. Note that your repo will signify that it was built from this template.
+      - Click [here](https://github.com/not-first/vite-biome-tailwind-template/generate), or the 'Use this template' green button in the top right.
+  
+2. Install dependencies though `npm install`.
+3. Alter for your needs. Remember to:
+   - Update the `LICENSE` to the correct license and name
+   - Change the `name` and `author` properties in `package.json`
+   - Clean up the `README.md`
+4. You are good to go. Start the project with `npm run dev` and you will see an altered Vite starting screen with Vite, React, Biome and Tailwind styled using Tailwind classes.
---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import biomeLogo from "./assets/biome.svg";
+import tailwindLogo from "./assets/tailwind.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-800 font-normal text-white leading-6 text-opacity-90">
+      <div className="m-0 flex min-w-[320px] max-w-screen-xl flex-col place-items-center justify-between p-8 text-center">
+        <div className="flex flex-row justify-center">
+          <a
+            href="https://vitejs.dev"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium"
+          >
+            <img
+              src={viteLogo}
+              alt="Vite logo"
+              className="h-32 p-6 transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-2xl hover:filter"
+            />
+          </a>
+          <a
+            href="https://react.dev"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium"
+          >
+            <img
+              src={reactLogo}
+              alt="React logo"
+              className="h-32 p-6 transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-2xl hover:filter"
+            />
+          </a>
+          <a
+            href="https://biomejs.dev/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium"
+          >
+            <img
+              src={biomeLogo}
+              alt="BiomeJS logo"
+              className="h-32 p-6 transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-2xl hover:filter"
+            />
+          </a>
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium"
+          >
+            <img
+              src={tailwindLogo}
+              alt="TailwindCSS logo"
+              className="h-32 p-6 transition-all duration-300 ease-in-out hover:scale-110 hover:drop-shadow-2xl hover:filter"
+            />
+          </a>
+        </div>
+        <h1 className="p-8 font-bold text-5xl">
+          Vite + React + Biome + Tailwind
+        </h1>
+        <div className="p-8">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            type="button"
+            className="cursor-pointer rounded-md border border-transparent bg-gray-900 px-3 py-2 font-medium text-base transition-colors duration-200 hover:border-indigo-500"
+          >
+            count is {count}
+          </button>
+          <p className="p-6">
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="text-gray-400">
+          Click on the Vite, React, Biome and Tailwind logos to learn more
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
